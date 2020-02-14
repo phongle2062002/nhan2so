3eb914b672d0d28330e0176f971ceda3b4a08271
phongle2062002 <phongle2062002@gmail.com>
2020-02-14 19:26:29 +0700

    phong le

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main(int argc, char** argv) {
+	float a,b,x;
+	cout<<"Nhap so a :";
+	cin>>a;
+	cout<<"Nhap so b :";
+	cin>>b;
+	x= a*b;
+	cout<<"Nhan 2 so "<<x;
+	return 0;
+}
